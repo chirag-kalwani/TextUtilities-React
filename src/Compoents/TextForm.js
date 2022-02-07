6e62b2fae2ea087f42491c8ede2a5a6768dabebc
Chirag Kalwani <75169388+Ch960@users.noreply.github.com>
2022-02-07 15:33:32 +0530

    added som more features in javascript

--- src/Compoents/TextForm.js
+++ src/Compoents/TextForm.js
@@ -8,21 +8,47 @@ TextForm.propTypes = {
 
 function TextForm(props) {
     const handleUpclick = () => {
-        console.log("Uppercase was clicked");
         setText(text.toUpperCase());
     }
+    const handlelowerClick = () => {
+        setText(text.toLowerCase());
+    }
+    const handleClearClick = () => {
+        var s = '';
+        setText(s);
+    }
     const handleOnchange = (event) => {
+
         setText(event.target.value);
     }
-    const [text, setText] = useState('Enter Text Here');
+    let countWord = () => {
+        return text.endsWith(" ") === true ? text.split(" ").length - 1 : text.split(" ").length
+    };
+    const [text, setText] = useState('');
     return (
-        <div>
-            <h1>{props.heading}</h1>
-            <div className="form-group">
-                <textarea className="form-control" value={text} onChange={handleOnchange} id="myBox" rows="8"/>
+        <>
+            <div className={"container"}>
+                <h1>{props.heading}</h1>
+                <div className="form-group">
+                    <textarea className="form-control" value={text} placeholder={'Enter Text Here'}
+                              onChange={handleOnchange} id="myBox" rows="8"/>
+                </div>
+                <button type="button" className="btn btn-success my-3 mx-1" onClick={handleUpclick}>Convert to UpperCase
+                </button>
+                <button type="button" className="btn btn-success my-3 mx-1" onClick={handlelowerClick}>Convert to
+                    LowerCase
+                </button>
+                <button type="button" className="btn btn-danger my-3 mx-1" onClick={handleClearClick}>Clear Text
+                </button>
             </div>
-            <button type="button" className="btn btn-success my-3" onClick={handleUpclick}>Convert to uppercase</button>
-        </div>
+            <div className="container my-2">
+                <h2> Your Text summary:</h2>
+                <p>{countWord()} words. {text.length} characters</p>
+                <p>Average time required to read: {Math.round((text.split(" ").length * 0.008) * 100) / 100} minutes</p>
+                <h2>Preview: </h2>
+                <p>{text}</p>
+            </div>
+        </>
     );
 }
 
