5655e14314eadc002123e13d3f4c0e84194658c8
Chirag Kalwani <75169388+Ch960@users.noreply.github.com>
2022-02-09 06:44:14 +0530

    Fix some bugs like alert

--- src/Compoents/NavBar.js
+++ src/Compoents/NavBar.js
@@ -14,7 +14,7 @@ function NavBar(props) {
                 <div className="container-fluid">
                     <Link className="navbar-brand" to="/">{props.title}</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"/>
                     </button>
--- src/Compoents/TextForm.js
+++ src/Compoents/TextForm.js
@@ -2,18 +2,10 @@ import React, {useState} from 'react';
 
 function TextForm(props) {
     const handleUpclick = () => {
-        if (text.length <= 0) {
-            props.showAlert('Enter the Text to Convert UpperCase!', 'warning');
-            return;
-        }
         props.showAlert('Converted to UpperCase!', 'success');
         setText(text.toUpperCase());
     }
     const handlelowerClick = () => {
-        if (text.length <= 0) {
-            props.showAlert('Enter the Text to Convert LowerCase!', 'warning');
-            return;
-        }
         props.showAlert('Converted to LowerCase!', 'success');
         setText(text.toLowerCase());
     }
@@ -30,14 +22,11 @@ function TextForm(props) {
         return text.endsWith(" ") === true ? text.split(/\s+/g).length - 1 : text.split(/\s+/g).length
     };
     const handleCopyText = () => {
-        if (text.length <= 0) {
-            props.showAlert('Enter Text to copy', 'warning');
-            return;
-        }
         props.showAlert('Text has been Copied', 'success');
         let text1 = document.getElementById('myBox');
         text1.select();
         navigator.clipboard.writeText(text1.value).then(r => "");
+        document.getSelection().removeAllRanges();
     }
     const [text, setText] = useState('');
     const changeTextColor = () => {
@@ -48,20 +37,24 @@ function TextForm(props) {
     return (
         <>
             <div className="container">
-                    <h1 className={`text-${changeTextColor()}`}>{props.heading}</h1>
+                <h1 className={`text-${changeTextColor()}`}>{props.heading}</h1>
                 <div className="form-group">
                     <textarea className={`form-control bg-${props.mode} text-${changeTextColor()}`} value={text}
                               placeholder={'Enter Text Here'}
                               onChange={handleOnchange} id="myBox" rows="8"/></div>
-                <button type="button" className="btn btn-success my-3 mx-1" onClick={handleUpclick}>Convert to
+                <button type="button" className="btn btn-success my-3 mx-1" onClick={handleUpclick}
+                        disabled={text.length === 0}>Convert to
                     UpperCase
                 </button>
-                <button type="button" className="btn btn-success my-3 mx-1" onClick={handlelowerClick}>Convert to
+                <button type="button" className="btn btn-success my-3 mx-1" onClick={handlelowerClick}
+                        disabled={text.length === 0}>Convert to
                     LowerCase
                 </button>
-                <button type="button" className="btn btn-danger my-3 mx-1" onClick={handleClearClick}>Clear Text
+                <button type="button" className="btn btn-danger my-3 mx-1" onClick={handleClearClick}
+                        disabled={text.length === 0}>Clear Text
                 </button>
-                <button type="button" className="btn btn-primary my-3 mx-1" onClick={handleCopyText}>Copy Text
+                <button type="button" className="btn btn-primary my-3 mx-1" onClick={handleCopyText}
+                        disabled={text.length === 0}>Copy Text
                 </button>
             </div>
             <div className="container my-2">
@@ -70,7 +63,7 @@ function TextForm(props) {
                 <p className={`text-${changeTextColor()}`}>Average time required to
                     read: {Math.round((text.length > 0 ? text.match(/\w/g).length * 0.008 : 0) * 100) / 100} minutes</p>
                 <h2 className={`text-${changeTextColor()}`}>Preview: </h2>
-                <p className={`text-${changeTextColor()}`}>{text.length > 0 ? text : 'Enter the text to preview.'}</p>
+                <p className={`text-${changeTextColor()}`}>{text.length > 0 ? text : 'Nothing to preview.'}</p>
             </div>
         </>
     );
